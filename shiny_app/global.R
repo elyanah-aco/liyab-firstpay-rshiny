c25563fb0b4e774ac759eb4b2a1b8b22fa2b4ab8
elyanah <elyanah.aco02@gmail.com>
2022-07-05 17:19:16 +0800

    edited cross-comparison; added intro text

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -3,6 +3,7 @@
 ##################################
 
 library(tidyverse)
+library(glue)
 
 ##################################
 ## Load data
@@ -11,10 +12,14 @@ library(tidyverse)
 liyab_df <- read.csv('liyab_cleaned.csv')
 
 ##################################
-## Assign variables
+## Assign variables and functions
 ##################################
 
 industry_choices <- sort(unique(liyab_df$industry))
 school_choices <- sort(unique(liyab_df$school_graduated))
 min_year <- min(liyab_df$year_of_first_job)
 max_year <- max(liyab_df$year_of_first_job)
+
+linebreaks <- function(n){
+  HTML(strrep(br(),n))
+}
--- shiny_app/server.R
+++ shiny_app/server.R
@@ -4,64 +4,79 @@
 ##################################
 
 server <- function(input, output, session){
+  
   df_filt_one <- reactive(liyab_df |> 
                             filter(industry == input$industry_one
                                    & school_graduated == input$school_one 
                                    & year_of_first_job >= input$date_slider_one[1] 
-                                   & year_of_first_job <= input$date_slider_one[2]))
+                                   & year_of_first_job <= input$date_slider_one[2]) |>
+                            filter(if(input$excl_negotiation == TRUE) (offer_negotiated != 'Yes') else TRUE))
   
   df_filt_two <- reactive(liyab_df |> 
                             filter(industry == input$industry_two
                                     & school_graduated == input$school_two 
                                     & year_of_first_job >= input$date_slider_two[1] 
-                                    & year_of_first_job <= input$date_slider_two[2]))
+                                    & year_of_first_job <= input$date_slider_two[2]) |>
+                            filter(if(input$excl_negotiation == TRUE) (offer_negotiated != 'Yes') else TRUE))
   
   output$salary_boxplot_one <- renderPlot({
-    if (input$incl_inflation == TRUE){
-      df_filt_one() |>
+      if (input$incl_inflation == TRUE){
+        df_filt_one() |>
         ggplot(aes(year_of_first_job, monthly_salary_adjusted)) +
-        theme_bw() + 
-        geom_boxplot(aes(factor(year_of_first_job)),
-                     fill = 'steelblue3',
-                     colour = 'grey30') +
-        labs(x = 'Year', y = 'Monthly (adjusted, in PHP)')
-    } else {
-      df_filt_one() |>
+          theme_bw() + 
+          geom_boxplot(aes(factor(year_of_first_job)),
+                   fill = 'steelblue3',
+                   colour = 'grey30') +
+          labs(title = glue('Monthly salary (', input$industry_one, 
+                          ', ', input$school_one, ')'),
+               subtitle = glue('Median salary: Php ', 
+                             median(df_filt_one()$monthly_salary_adjusted)),
+               x = 'Year', 
+               y = 'Monthly salary (adjusted, in PHP)')
+      } else {
+        df_filt_one() |>
         ggplot(aes(year_of_first_job, monthly_salary_unadjusted)) +
-        theme_bw() + 
-        geom_boxplot(aes(factor(year_of_first_job)), 
-                     fill = 'steelblue3',
-                     colour = 'grey30') + 
-        labs(x = 'Year', y = 'Monthly (unadjusted, in PHP)')
-    }
+          theme_bw() + 
+          geom_boxplot(aes(factor(year_of_first_job)),
+                       fill = 'steelblue3',
+                       colour = 'grey30') +
+          labs(title = glue('Monthly salary (', input$industry_one, 
+                          ', ', input$school_one, ')'),
+               subtitle = glue('Median salary: Php ', 
+                               median(df_filt_one()$monthly_salary_unadjusted)),
+               x = 'Year', 
+               y = 'Monthly salary (adjusted, in PHP)')
+      }
   }, res = 96)
   
   
   output$salary_boxplot_two <- renderPlot({
-    if (input$incl_inflation == TRUE){
-      df_filt_two() |>
+      if (input$incl_inflation == TRUE){
+        df_filt_two() |>
         ggplot(aes(year_of_first_job, monthly_salary_adjusted)) +
+          theme_bw() + 
+          geom_boxplot(aes(factor(year_of_first_job)),
+                       fill = 'salmon2',
+                       colour = 'grey10') + 
+          labs(title = glue('Monthly salary (', input$industry_two, 
+                          ', ', input$school_two, ')'),
+               subtitle = glue('Median salary: Php ', 
+                           median(df_filt_two()$monthly_salary_adjusted)),
+               x = 'Year', 
+               y = 'Monthly salary (adjusted, in PHP)')
+    } else {
+      df_filt_two() |>
+      ggplot(aes(year_of_first_job, monthly_salary_unadjusted)) +
         theme_bw() + 
         geom_boxplot(aes(factor(year_of_first_job)),
                      fill = 'salmon2',
-                     colour = 'grey10') +
-        labs(x = 'Year', y = 'Monthly salary (adjusted, in PHP)')
-    } else {
-      df_filt_two() |>
-        ggplot(aes(year_of_first_job, monthly_salary_unadjusted)) +
-        theme_bw() + 
-        geom_boxplot(aes(factor(year_of_first_job)), 
-                     fill = 'salmon2',
                      colour = 'grey10') + 
-        labs(x = 'Year', y = 'Monthly salary (unadjusted, in PHP)')
+      labs(title = glue('Monthly salary (', input$industry_two, 
+                        ', ', input$school_two, ')'),
+           subtitle = glue('Median salary: Php ', 
+                           median(df_filt_two()$monthly_salary_unadjusted)),
+           x = 'Year', 
+           y = 'Monthly salary (adjusted, in PHP)')
     }
   }, res = 96)
-  
-  output$median_text <- renderText({
-    if (input$incl_inflation == TRUE){
-      median(df_filt_one()$monthly_salary_adjusted)
-    } else {
-      median(df_filt_one()$monthly_salary_unadjusted)
-    }
-  })
 }
