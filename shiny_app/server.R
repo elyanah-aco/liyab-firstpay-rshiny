dd0f8bce4b7fc8a9e41e933ce4b67bcd83a99ebe
elyanah <elyanah.aco02@gmail.com>
2022-07-05 11:55:00 +0800

    cleaned code; added cross-industry and cross-university comparison for salaries

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -4,23 +4,64 @@
 ##################################
 
 server <- function(input, output, session){
-  df_filt <- reactive(liyab_df |> 
-                        filter(industry == input$industry 
-                               & school_graduated == input$school 
-                               & year_of_first_job >= input$date_slider[1] 
-                               & year_of_first_job <= input$date_slider[2]))
+  df_filt_one <- reactive(liyab_df |> 
+                            filter(industry == input$industry_one
+                                   & school_graduated == input$school_one 
+                                   & year_of_first_job >= input$date_slider_one[1] 
+                                   & year_of_first_job <= input$date_slider_one[2]))
   
-  output$avg_salary_per_year <- renderPlot({
+  df_filt_two <- reactive(liyab_df |> 
+                            filter(industry == input$industry_two
+                                    & school_graduated == input$school_two 
+                                    & year_of_first_job >= input$date_slider_two[1] 
+                                    & year_of_first_job <= input$date_slider_two[2]))
+  
+  output$salary_boxplot_one <- renderPlot({
     if (input$incl_inflation == TRUE){
-      df_filt() |>
+      df_filt_one() |>
         ggplot(aes(year_of_first_job, monthly_salary_adjusted)) +
-        geom_boxplot(aes(factor(year_of_first_job))) +
-        labs(x = 'Year', y = 'Distribution of monthly salary (adjusted, in PHP)')
+        theme_bw() + 
+        geom_boxplot(aes(factor(year_of_first_job)),
+                     fill = 'steelblue3',
+                     colour = 'grey30') +
+        labs(x = 'Year', y = 'Monthly (adjusted, in PHP)')
     } else {
-      df_filt() |>
+      df_filt_one() |>
         ggplot(aes(year_of_first_job, monthly_salary_unadjusted)) +
-        geom_boxplot(aes(factor(year_of_first_job))) + 
-        labs(x = 'Year', y = 'Distribution of monthly salary (unadjusted, in PHP)')
+        theme_bw() + 
+        geom_boxplot(aes(factor(year_of_first_job)), 
+                     fill = 'steelblue3',
+                     colour = 'grey30') + 
+        labs(x = 'Year', y = 'Monthly (unadjusted, in PHP)')
     }
   }, res = 96)
+  
+  
+  output$salary_boxplot_two <- renderPlot({
+    if (input$incl_inflation == TRUE){
+      df_filt_two() |>
+        ggplot(aes(year_of_first_job, monthly_salary_adjusted)) +
+        theme_bw() + 
+        geom_boxplot(aes(factor(year_of_first_job)),
+                     fill = 'salmon2',
+                     colour = 'grey10') +
+        labs(x = 'Year', y = 'Monthly salary (adjusted, in PHP)')
+    } else {
+      df_filt_two() |>
+        ggplot(aes(year_of_first_job, monthly_salary_unadjusted)) +
+        theme_bw() + 
+        geom_boxplot(aes(factor(year_of_first_job)), 
+                     fill = 'salmon2',
+                     colour = 'grey10') + 
+        labs(x = 'Year', y = 'Monthly salary (unadjusted, in PHP)')
+    }
+  }, res = 96)
+  
+  output$median_text <- renderText({
+    if (input$incl_inflation == TRUE){
+      median(df_filt_one()$monthly_salary_adjusted)
+    } else {
+      median(df_filt_one()$monthly_salary_unadjusted)
+    }
+  })
 }
